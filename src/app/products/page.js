42b81f08952a29b6f62b9e1cbc54e20b114aef2c
jamilnim <jamilnimbook2@gmail.com>
2026-02-15 01:16:10 +0200

    hh

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { supabase } from "../../lib/supabase";
 import ProductCard from "../../components/ProductCard";
 import styles from "./Products.module.css";
