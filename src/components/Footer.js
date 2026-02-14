47bb351399eca4aa6198131804e1027b689d5ab0
jamilnim <jamilnimbook2@gmail.com>
2026-02-14 11:59:05 +0200

    logo at footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <h3>JUSTE BAG SOURCING</h3>
+        <div>
+          <Link href="/">
+            <img
+              src="/pageimage/okoLogo.png
+            "
+              alt="Oko Sourcing Logo"
+              className={styles.logoImg}
+            />
+          </Link>
+        </div>
         <p>Reliable manufacturing partner for sustainable bag production.</p>
-        <p>© {new Date().getFullYear()} Juste Bag. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} Oko-mode. All rights reserved.</p>
       </div>
     </footer>
   );
