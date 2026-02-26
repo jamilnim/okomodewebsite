a9d9db73157a6667b3fdbedb59fb1bbd30f698b5
jamilnim <jamilnimbook2@gmail.com>
2026-02-26 23:59:38 +0200

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,21 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://oko-mode.com",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://oko-mode.com/about",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://oko-mode.com/contact",
+        lastModified: new Date(),
+      },
+      {
+        url: "https://oko-mode.com/sustainability",
+        lastModified: new Date(),
+      },
+    ];
+  }
+  
